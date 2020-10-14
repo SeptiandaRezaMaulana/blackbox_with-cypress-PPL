117f0c6107d9645cfb7ba4606953f02b4d87f0c5
SeptiandaRezaMaulana <septiandareza07@gmail.com>
2020-10-14 12:46:32 +0700

    done case inline form

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,9 +1,30 @@
 /// <reference types="cypress" />
 
 describe('My first test suite', () => {
+    // Case 1 Inline form
     it('first test', () => {
         cy.visit('/')
         cy.contains('Forms').click();
         cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Inline form')
+            .find('[placeholder="Jane Doe"]')
+            .click()
+            .type("Septianda Reza Maulana")
+
+        cy.contains('nb-card', 'Inline form')
+            .find('[placeholder="Email"]')
+            .click()
+            .type("septiandareza07@gmail.com")
+
+        cy.contains('nb-card', 'Inline form')
+            .find('.custom-checkbox')
+            .click()
+
+        cy.contains('nb-card', 'Inline form')
+            .find('[type="submit"]')
+            .click()
+        
+        cy.url().should('include', 'forms/layouts')
     })
 })
