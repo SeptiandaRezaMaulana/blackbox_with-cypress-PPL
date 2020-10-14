c60d22ce43bc77ea1871969f97a3dd3e34f06735
SeptiandaRezaMaulana <septiandareza07@gmail.com>
2020-10-14 22:50:43 +0700

    done case using the grid

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -27,4 +27,34 @@ describe('My first test suite', () => {
         
         cy.url().should('include', 'forms/layouts')
     })
+
+    // Case 2 Using the Grid
+    it('second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click();
+        cy.contains('Form Layouts').click()
+
+        //contains disini harus sama besar kecilnya huruf
+        cy.contains('nb-card', 'Using the Grid') 
+            .find('[data-cy="imputEmail1"]')
+            .click()
+            .type("septiandareza07@gmail.com")
+
+        cy.get('#inputPassword2')
+            .click()
+            .type("septianda")
+
+        cy.contains('nb-card', 'Using the Grid')
+            // Pada bagian ini memilih salah satu option dimana terletak pada radios    
+            .contains("Option 2") 
+            .parent('nb-radio')
+            .find('.inner-circle')
+            .click()
+
+        cy.contains('nb-card', 'Using the Grid')
+            .find('[type="submit"]')
+            .click()
+
+        cy.url().should('include', 'forms/layout')
+    })
 })
